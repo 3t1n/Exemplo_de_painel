239d6265d956a1ee76d320e7ef3f92e6840d024e
reynaldo <reynaldotadeu10@gmail.com>
2018-04-23 20:54:08 -0300

    vendedores html

--- resources/views/vendedores/vendedores.blade.php
+++ resources/views/vendedores/vendedores.blade.php
@@ -11,6 +11,12 @@
 
             <div class="container">
   <form>
+  <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">nome</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputName3" placeholder="nome">
+      </div>
+    </div>
     <div class="form-group row">
       <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
       <div class="col-sm-10">
@@ -18,42 +24,9 @@
       </div>
     </div>
     <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
       <div class="col-sm-10">
         <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-      </div>
-    </div>
-    <fieldset class="form-group row">
-      <legend class="col-form-legend col-sm-2">Radios</legend>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <label class="form-check-label">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-            Option one is this and that&mdash;be sure to include why it's great
-          </label>
-        </div>
-        <div class="form-check">
-          <label class="form-check-label">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-            Option two can be something else and selecting it will deselect option one
-          </label>
-        </div>
-        <div class="form-check disabled">
-          <label class="form-check-label">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-            Option three is disabled
-          </label>
-        </div>
-      </div>
-    </fieldset>
-    <div class="form-group row">
-      <label class="col-sm-2">Checkbox</label>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <label class="form-check-label">
-            <input class="form-check-input" type="checkbox"> Check me out
-          </label>
-        </div>
       </div>
     </div>
     <div class="form-group row">
