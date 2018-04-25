e6f19dcf22001dc2a4429eb392bfd0e01333e320
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 12:59:18 -0300

    post

--- resources/views/produtos/produtos.blade.php
+++ resources/views/produtos/produtos.blade.php
@@ -1,20 +1,14 @@
 @extends('layouts.painel')
 @section('content')
 
+
     <div class="title">
-        Produtos
+        Fornecedores
     </div>
-    <div class="main">
-        <div class="widget">
-            <div class="title">Número de Produtos</div>
-            <div class="chart">
-
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </div>
+    <div class="card">
+        <div class="card-header">Número de Vendedores</div>
+        <div class="card-body">
         </div>
-
     </div>
 @endsection
 
