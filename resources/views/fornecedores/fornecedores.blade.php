4642cd1b407085b2f987e492f695fb5fbea86d82
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 14:46:58 -0300

    post

--- resources/views/fornecedores/fornecedores.blade.php
+++ resources/views/fornecedores/fornecedores.blade.php
@@ -4,6 +4,8 @@
     <div class="title">
         Fornecedores
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
     <div class="card">
             <div class="card-header font-weight-bold">Adicionar Fornecedor</div>
         <div class="card-body">
@@ -30,10 +32,16 @@
                     </div>
                     <div class="form-group col-md-6">
                         <label for="telefone" class="font-weight-bold">Telefone</label>
-                        <input type="text" class="form-control" data-mask="(00) 00000-0000" id="telefone" name="telefone" placeholder="Digite o seu Telefone com DDD*">
+                        <input type="text" class="form-control" data-mask="(00) 00000-0000" id="telefone" name="telefone" placeholder="Digite o seu Telefone com DDD*" maxlength="12">
                     </div>
                 </div>
             </form>
+            <script>
+                $(document).ready(function () {
+                    var $seuCampoCpf = $("#inputRegistro");
+                    $seuCampoCpf.mask('000.000.000-00', {reverse: true});
+                });
+            </script>
         </div>
     </div>
 
