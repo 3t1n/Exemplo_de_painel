c100d2a85f7c54e1539f25f2d12fdb94e104973d
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 11:58:58 -0300

    post

--- resources/views/vendedores/vendedores.blade.php
+++ resources/views/vendedores/vendedores.blade.php
@@ -5,42 +5,39 @@
         Vendedores
     </div>
         <div class="widget">
-            <div class="title">Número de Vendedores</div>
-            <div class="chart">
-            <div class="container">
-                @if (Session::has('mensagem_sucesso_vendedor'))
-                    <div class="alert alert-success">{{ Session::get('mensagem_sucesso_vendedor') }} </div>
-                @endif
-  <form method="POST" action="{{ route('adcVendedor') }}">
-      @csrf
+                            <div class="title">Número de Vendedores</div>
+                            <div class="container">
+                                @if (Session::has('mensagem_sucesso_vendedor'))
+                                    <div class="alert alert-success">{{ Session::get('mensagem_sucesso_vendedor') }} </div>
+                                @endif
+                  <form method="POST" action="{{ route('adcVendedor') }}">
+                      @csrf
 
-      <div class="form-group row">
-      <label for="inputname" class="col-sm-2 col-form-label">nome</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" id="name" name='name' placeholder="nome">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-      <div class="col-sm-10">
-        <input type="email" class="form-control" id="email" name='email' placeholder="Email">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
-      <div class="col-sm-10">
-        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
-      </div>
-    </div>
-    <div class="form-group row">
-      <div class="offset-sm-2 col-sm-10">
-        <button type="submit" class="btn btn-primary">adcionar</button>
-      </div>
-    </div>
-  </form>
-</div>
-            </div>
+                      <div class="form-group row">
+                      <label for="inputname" class="col-sm-2 col-form-label">nome</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name='name' placeholder="nome">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                      <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" name='email' placeholder="Email">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="offset-sm-2 col-sm-10">
+                        <button type="submit" class="btn btn-primary">adcionar</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
         </div>
-
 @endsection
 
