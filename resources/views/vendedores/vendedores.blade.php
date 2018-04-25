a4f2b56f219bba2f1545f990de2c46013d0c5f83
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 11:03:06 -0300

    post

--- resources/views/vendedores/vendedores.blade.php
+++ resources/views/vendedores/vendedores.blade.php
@@ -10,25 +10,25 @@
             <div class="chart">
 
             <div class="container">
-  <form method="POST" action="">
+  <form method="POST">
       @csrf
 
       <div class="form-group row">
       <label for="inputname" class="col-sm-2 col-form-label">nome</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control" id="inputName" placeholder="nome">
+        <input type="text" class="form-control" id="name" name='nome' placeholder="nome">
       </div>
     </div>
     <div class="form-group row">
       <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
       <div class="col-sm-10">
-        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+        <input type="email" class="form-control" id="email" name='email' placeholder="Email">
       </div>
     </div>
     <div class="form-group row">
       <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
       <div class="col-sm-10">
-        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        <input type="password" class="form-control" id="password" name='password' placeholder="Password">
       </div>
     </div>
     <div class="form-group row">
