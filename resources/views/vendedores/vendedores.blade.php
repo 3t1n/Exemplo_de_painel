b03fabf527282080c82b929c4cf1ac7295ca6e6a
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 12:36:09 -0300

    post

--- resources/views/vendedores/vendedores.blade.php
+++ resources/views/vendedores/vendedores.blade.php
@@ -15,25 +15,25 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="inputname" class="col-form-label">nome</label>
-                            <div class="col-form-label">
+                            <label for="inputname" class="col-sm-2 col-form-label">nome</label>
+                            <div class="col-sm-10">
                                 <input type="text" class="form-control" id="name" name='name' placeholder="nome">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputEmail3" class="col-form-label">Email</label>
-                            <div class="col-form-label">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
                                 <input type="email" class="form-control" id="email" name='email' placeholder="Email">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputPassword3" class="col-form-label">Senha</label>
-                            <div class="col-form-label">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
+                            <div class="col-sm-10">
                                 <input type="password" class="form-control" id="password" name='password' placeholder="Password">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="col-form-label">
+                            <div class="offset-sm-2 col-sm-10">
                                 <button type="submit" class="btn btn-primary">adcionar</button>
                             </div>
                         </div>
