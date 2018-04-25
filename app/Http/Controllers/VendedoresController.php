a85b4af05fffae8fe395e8bd94281056e02c7151
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 11:29:37 -0300

    post

--- app/Http/Controllers/VendedoresController.php
+++ app/Http/Controllers/VendedoresController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Vendedores;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class VendedoresController extends Controller
 {
@@ -12,11 +13,17 @@ class VendedoresController extends Controller
     }
     public function registrar(Request $request){
 
+        $nome = $request->only(['name']);
+        $email = $request->only(['email']);
+        $senha = $request->only(['password']);
+        $senhaCrip = Hash::make($senha);
+        return response()->json($nome,$email,$senhaCrip, 201);
+        /*
         $vendedor = new Vendedores();
         $vendedor->fill($request->all());
         $vendedor->save();
         return response()->json($vendedor, 201);
 
-
+*/
     }
 }
