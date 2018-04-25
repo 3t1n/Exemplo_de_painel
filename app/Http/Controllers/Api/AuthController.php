25e1a849a5d4aa737a970234f85866de0a534b4d
reynaldo <reynaldotadeu10@gmail.com>
2018-04-25 10:37:50 -0300

    certo

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Support\Facades\Config;
 
 
@@ -16,21 +17,17 @@ class AuthController extends Controller
 
     private $jwtAuth;
 
-    public $jwtconf = eval("Config::set('jwt.user', 'App\Vendedores');");
-    public $authconf = eval("Config::set('auth.providers.users.model', \App\Vendedores::class);");
 
     public function __construct(JWTAuth $jwtAuth)
     {
-        $this->jwtconf;
-        $this->authconf;
-
         $this->jwtAuth = $jwtAuth;
     }
 
     public function login(Request $request)
     {
-        $this->jwtconf;
-        $this->authconf;
+        Config::set('jwt.user', 'App\Vendedores');
+        Config::set('auth.providers.users.model', \App\Vendedores::class);
+
         $credentials = $request->only('email', 'password');
 
             if (! $token = $this->jwtAuth->attempt($credentials)) {
@@ -40,16 +37,14 @@ class AuthController extends Controller
         return response()->json(compact('token','user'));
     }
     public function refresh(){
-        $this->jwtconf;
-        $this->authconf;
+
         $token = $this->jwtAuth->getToken();
         $token = $this->jwtAuth->refresh($token); /*resultado do refresh*/
 
         return response()->json(compact('token'));
     }
     public function logout(){
-        $this->jwtconf;
-        $this->authconf;
+
         $token = $this->jwtAuth->getToken();
         $this->jwtAuth->invalidate($token); //invalidando o token
 
@@ -57,8 +52,6 @@ class AuthController extends Controller
     }
     public function me()
     {
-        $this->jwtconf;
-        $this->authconf;
 
             if (! $user = $this->jwtAuth->parseToken()->authenticate()) {
                 return response()->json(['error' => 'user_not_found'], 404);
