b6a55bd80045bc1e7f21433d2dd204e2b57a9485
reynaldo <reynaldotadeu10@gmail.com>
2018-04-28 22:34:18 -0300

    mais novo

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
 $url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-*/
+
 return [
 
     /*
@@ -20,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'pgsql_producao'),
 
     /*
     |--------------------------------------------------------------------------
@@ -73,7 +73,7 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-/*
+
         'pgsql_producao' => array(
             'driver'   => 'pgsql',
             'host'     => $host,
@@ -84,7 +84,7 @@ return [
             'prefix'   => '',
             'schema'   => 'public',
         ),
-*/
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
